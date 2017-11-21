a550054d70bde8a5cfe240f93e23b875fdcb4ace
VoleResistor <VoleResistor@gmail.com>
2017-11-21 11:28:53 -0600

    Convert Get-AdSites to function. Create Get-AdForestObject. Update README.

--- Active Directory/Get-ADSites.ps1
+++ Active Directory/Get-ADSites.ps1
@@ -1,17 +1,47 @@
-﻿[cmdletbinding()]            
-param()            
+﻿function Get-AdSites
+{
+    <#
+    .SYNOPSIS
+    Return information about AD sites in the specified forest.
+    
+    .DESCRIPTION
+    Use .NET to gather detailed AD site information in the specified forest. This function also relies on Get-AdForestObject.
+    
+    .PARAMETER DomainName
+    Name of the forest domain to gather data from. If not specified, the current forest is used.
+    
+    .EXAMPLE
+    Get-AdSites -DomainName example.com
 
-$Sites = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest().Sites           
-$obj = @() 
-foreach ($Site in $Sites) {            
+    Get detailed site information from the example.com forest.
 
- $obj += New-Object -Type PSObject -Property (            
-  @{            
-   "SiteName"  = $site.Name;            
-   "SubNets" = $site.Subnets;            
-   "Servers" = $Site.Servers;
-   "SiteLinks" = $site.SiteLinks.Name          
-  }            
- )            
+    .EXAMPLE
+    Get-AdSites
+
+    Get detailed site information from the current forest.
+    
+    .NOTES
+    Main process borrowed from another website. I didn't make a note of the source at the time.
+    #>
+    [cmdletbinding()]            
+    param
+    (
+        [Parameter(Mandatory=$false)]
+        [string]$DomainName
+    )            
+
+    $Sites = (Get-ADForestObject $DomainName).Sites           
+    $obj = @() 
+    foreach ($Site in $Sites)
+    {            
+        $obj += New-Object -Type PSObject -Property(            
+            @{            
+                "SiteName"  = $site.Name;            
+                "SubNets" = $site.Subnets;            
+                "Servers" = $Site.Servers;
+                "SiteLinks" = $site.SiteLinks.Name          
+            }            
+        )            
+    }
+    return $obj
 }
-$obj
---
+++ Active Directory/Get-AdForestObject.ps1
@@ -0,0 +1,53 @@
+function Get-ADForestObject
+{
+    <#
+    .SYNOPSIS
+    Get information about the specified forest.
+    
+    .DESCRIPTION
+    Use .NET objects to gather detailed forest information.
+    
+    .PARAMETER ForestName
+    FQDN of the target forest. If not specified, the current forest is used.
+    
+    .PARAMETER Credential
+    A PSCredential object containing a username and hashed password.
+    
+    .EXAMPLE
+    Get-ADForestObject
+
+    Get forest info for the current forest.
+    
+    .NOTES
+    Borrowed from Edge Pereira at http://www.superedge.net/2012/09/how-to-get-ad-forest-in-powershell.html
+    #>
+    param
+    (
+        [Parameter(Mandatory=$false, Position=1)]
+        [string]$ForestName,
+
+        [Parameter(Mandatory=$false)]
+        [System.Management.Automation.PsCredential]$Credential
+    )
+
+    #if forest is not specified, get current context forest
+    If (!$ForestName)     
+    {
+        $ForestName = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest().Name.ToString()    
+    }        
+
+    If ($Credential)     
+    {        
+        $credentialUser = $Credential.UserName.ToString()
+        $credentialPassword = $Credential.GetNetworkCredential().Password.ToString()
+        $adCtx = New-Object System.DirectoryServices.ActiveDirectory.DirectoryContext("forest", $ForestName, $credentialUser, $credentialPassword )
+    }    
+    Else     
+    {        
+        $adCtx = New-Object System.DirectoryServices.ActiveDirectory.DirectoryContext("forest", $ForestName)    
+    }        
+
+    $output = ([System.DirectoryServices.ActiveDirectory.Forest]::GetForest($adCtx))    
+
+    Return $output
+}
