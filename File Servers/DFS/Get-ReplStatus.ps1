f3d818dd15900031c4644ea0c678b6c96129ffca
VoleResistor <VoleResistor@gmail.com>
2017-12-01 08:40:45 -0600

    Remove Count-ErrorLogs.ps1, Export-DFSStructure.ps1, Get-DfsrHotfixes.ps1, Get-ParsedRootLog.ps1, Get-PerfCounters.ps1. Complete rewite of Get-ReplStatus.ps1 to simplify operation.

--- File Servers/DFS/Get-ReplStatus.ps1
+++ File Servers/DFS/Get-ReplStatus.ps1
@@ -1,50 +1,52 @@
-﻿param
-(
-    [parameter(
-        ValueFromPipeline=$true,
-        ValueFromPipelineByPropertyName=$true
-    )]
-    [string[]]$ComputerName = "$env:computername",
+﻿function Get-ReplStatus
+{
+    <#
+    .SYNOPSIS
+    Quickly gather DFS replication status.
     
-    [switch]$List
-)
+    .DESCRIPTION
+    Get a count of backlogged files on each given DFSR server. Can also get a full listing of files in backlogs.
+    
+    .PARAMETER ComputerName
+    An array of computernames to check.
+    
+    .PARAMETER List
+    Get a complete listing of backlogged files instead of just a count.
+    
+    .EXAMPLE
+    Get-ReplStatus -ComputerName DFSServer01,DFSServer02 -List
 
-begin
-{
-    $AllMembersList     = @()
-    $AllMembersCount    = @()
-}
+    Get a complete listing of the backlogs on DFSServer01 and DFSServer02.
+    #>
+    [CmdletBinding()]
+    param
+    (
+        [parameter(Mandatory=$true, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true, Position=1)]
+        [string[]]$ComputerName,
 
-process
-{
-    foreach ($target in $ComputerName)
+        [Parameter(Mandatory=$false)]
+        [switch]$List
+    )
+
+    $Results = @()
+
+    foreach ($c in $ComputerName)
     {
-        $BackLog = Get-DfsrState -ComputerName $target
-        
+        $BackLog = Get-DfsrState -ComputerName $c -ErrorAction SilentlyContinue
         if ($List)
         {
-            $AllMembersList += $BackLog
+            $Results += $BackLog
         }
         else
         {
             $BackLogCount = New-Object -TypeName psobject
-            $BackLogCount | Add-Member -MemberType NoteProperty -Name ComputerName -Value $target
-            $BackLogCount | Add-Member -MemberType NoteProperty -Name InboundCount -Value (($BackLog | ?{$_.Inbound -eq 'True'}).Count)
-            $BackLogCount | Add-Member -MemberType NoteProperty -Name OutboundCount -Value (($BackLog | ?{$_.Inbound -ne 'True'}).Count)
+            $BackLogCount | Add-Member -MemberType NoteProperty -Name ComputerName -Value $c
+            $BackLogCount | Add-Member -MemberType NoteProperty -Name InboundCount -Value (($BackLog | Where-Object {$_.Inbound -eq 'True'}).Count)
+            $BackLogCount | Add-Member -MemberType NoteProperty -Name OutboundCount -Value (($BackLog | Where-Object {$_.Inbound -ne 'True'}).Count)
             $BackLogCount | Add-Member -MemberType NoteProperty -Name Total -Value (($BackLog).Count)
-            $AllMembersCount += $BackLogCount
+            $Results += $BackLogCount
         }
     }
-}
 
-end
-{
-    if ($List)
-    {
-        return $AllMembersList | Select-Object PSComputerName,SourceComputerName,FileName,Path,Inbound,UpdateState
-    }
-    else
-    {
-        return $AllMembersCount
-    }
+    return $Results
 }
